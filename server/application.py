ad66ce8e4f4ca547e410feb205da43e77ed86204
torenDM <demidovmatvey@rambler.ru>
2026-01-15 17:01:45 +0500

    Lab11: app + docker + k8s + actions

---
+++ server/application.py
@@ -0,0 +1,27 @@
+﻿import http.server
+import socketserver
+
+PORT = 8000
+
+
+class TestMe:
+    """Small class for unit tests."""
+
+    def take_five(self) -> int:
+        """Return constant 5."""
+        return 5
+
+    def port(self) -> int:
+        """Return server port."""
+        return PORT
+
+
+def main() -> None:
+    handler = http.server.SimpleHTTPRequestHandler
+    with socketserver.TCPServer(("", PORT), handler) as httpd:
+        print("serving at port", PORT)
+        httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    main()
